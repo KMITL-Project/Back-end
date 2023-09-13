46532bfbaf401e6718ad42d4f4ab38c630a7b4dd
Kajonsak005 <64015016@kmitl.ac.th>
2023-09-13 15:16:48 +0700

    init proejct: add structure file : add automate testing

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import config from '~/config'
+import { getConnection } from './packages/database'
+import server from './server'
+
+const PORT = config.SERVER_PORT || '3000'
+
+async function onStart(): Promise<any> {
+  try {
+    await getConnection()
+  } catch (err) {
+    // tslint:disable-next-line:no-console
+    console.log(err)
+    throw err
+  }
+}
+
+// const currentServer = https.createServer(
+//   {
+//     cert: fs.readFileSync(`${__dirname}/../server.cert`, 'utf8'),
+//     key: fs.readFileSync(`${__dirname}/../server.key`, 'utf8'),
+//   },
+//   server,
+// )
+
+server.listen(PORT, onStart)
+
+// tslint:disable-next-line:no-console
+console.log(`Server up and running on https://localhost:${PORT}`)
